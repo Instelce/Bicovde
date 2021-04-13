52cc0c068c13d570554b32ae734cb70cf3c47ce9
Célestin <intelce.developpement@gmail.com>
2021-04-13 11:59:58 +0200

    All style finish

---
+++ main/js/convertions.js
@@ -0,0 +1,24 @@
+const sequence = {
+    '1':'2048',
+    '2': '1024',
+    '3': '512',
+    '4': '256',
+    '5': '128',
+    '6': '64',
+    '7': '32',
+    '8': '16',
+    '9': '8',
+    '10': '4',
+    '11': '2',
+    '12': '1',
+}
+
+// Convertion binary to decimal
+var request2t10 = document.getElementById('2t10').value;
+
+function converterBinaryToDecimal() {
+    alert(request2t10)
+}
+
+
+// Convertion decimal to binary
---
+++ main/js/script.js
@@ -0,0 +1,14 @@
+// For Swup
+// const swup = new Swup();
+
+// For modalbox
+const openModalboxButton = document.querySelector('.open__modalbox')
+const closeModalboxButton = document.querySelector('.modalbox__close')
+const modalbox = document.querySelector('.modalbox__container')
+openModalboxButton.addEventListener('click', () => {
+    modalbox.classList.add('modalbox__visible')
+})
+closeModalboxButton.addEventListener('click', () => {
+    modalbox.classList.remove('modalbox__visible')
+})
+
